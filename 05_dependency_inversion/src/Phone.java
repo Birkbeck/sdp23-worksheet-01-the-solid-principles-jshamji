6bc701007d745dddd14adad46298b3ca600b1397
jshamji <91161414+jshamji@users.noreply.github.com>
2023-01-25 17:04:03 +0000

    Created two new interfaces for Emailer.java and Phone.java. Also amended WeatherTracker.java.

--- 05_dependency_inversion/src/Phone.java
+++ 05_dependency_inversion/src/Phone.java
@@ -1,4 +1,5 @@
-public class Phone {
+public class Phone implements IPhone {
+    @Override
     public String generateWeatherAlert(String weatherConditions) {
         String alert = "It is " + weatherConditions;
         return alert;
